41af13896d886f6985c2da5c9219db03f080ba54
CoreyColgan <54746075+CoreyColgan@users.noreply.github.com>
2019-09-02 13:30:10 -0400

    Update assignment1_Part2.py

--- assignment1_Part2.py
+++ assignment1_Part2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[60]:
-
-
 #IS211 Week1 Part 2  - Simple Class
 
 # Create a class called Book -- This class should have the following properties:
@@ -18,10 +15,6 @@ class Book:
     def display(self):
        print (self.title,"written by",self.author)
 
-
-# In[61]:
-
-
 # Instantiate two objects from this class. 
 # The first object represents the book ‘Of Mice and Men’,written by John Steinbeck 
 # the other is ‘To Kill a Mockingbird’ by Harper Lee.
@@ -33,16 +26,3 @@ book2 = Book("To Kill a Mockingbird", "Harper Lee")
 
 book1.display()
 book2.display()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
