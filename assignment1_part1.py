406cd82a14565b98c5a74848e77633557c842665
CoreyColgan <54746075+CoreyColgan@users.noreply.github.com>
2019-09-02 12:59:49 -0400

    Add files via upload

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -4,12 +4,16 @@
 # In[82]:
 
 
+# Create a new function called listDivide that takes in two parameters, a list called numbers 
+# and an integer called divide that defaults to 2
 def listDivide(numbers, divide = 2):
     counter = 0
     for number in numbers:
         if(number % divide == 0):
             counter = counter + 1
     return counter
+
+# Create a custom exception class called ListDivideException 
 class ListDivideException(Exception):
     pass
 
@@ -17,6 +21,7 @@ class ListDivideException(Exception):
 # In[83]:
 
 
+# Create another function called testListDivide that performs various tests on ListDivide
 def testListDivide():
     print (listDivide([1,2,3,4,5]) == 2)
     print (listDivide([2,4,6,8,10]) == 5)
